38002c14229fbb7e82c7d34debeddc38e804b5b5
Heitor Polidoro <14806300+heitorpolidoro@users.noreply.github.com>
2021-12-16 14:12:25 -0300

    better-comments (#15)
    * better-comments
    
    * better-comments
    
    * linter

--- entrypoint.py
+++ entrypoint.py
@@ -6,6 +6,36 @@ import subprocess
 from github import Github
 
 
+class LintError:
+    def __init__(self, filename, error):
+        self.filename = filename
+        self.error = error
+        self._comment = []
+        self._formated_comment = None
+        self._code = None
+
+    @property
+    def code(self):
+        if self._code is None:
+            self._code = '\n'.join(self._comment[:-1])
+        return self._code
+
+    @property
+    def comment(self):
+        if self._formated_comment is None:
+            comments = '\n'.join(self._comment)
+            self._formated_comment = f"""
+{self.error}
+```
+{comments}
+```
+"""
+        return self._formated_comment
+
+    def add_comment_line(self, line):
+        self._comment.append(line)
+
+
 def already_commented(file, diff_index, body, comments):
     for comment in comments:
         if file.filename == comment.path and diff_index == comment.position and body == comment.body:
@@ -14,33 +44,29 @@ def already_commented(file, diff_index, body, comments):
 
 
 def run_flake():
-    flake_cmd = 'flake8 --show-source ' + os.environ['INPUT_FLAKE_PARAMETERS']
+    flake_cmd = 'flake8 --show-source ' + os.environ.get('INPUT_FLAKE_PARAMETERS', '')
     print(flake_cmd)
     returncode, outs = subprocess.getstatusoutput(flake_cmd)
     print(outs)
-    return outs, returncode
+    lint_error = None
+    errors = {}
+    for line in outs.split('\n'):
+        if line.startswith('./'):
+            if lint_error:
+                errors[lint_error.filename] = errors.get(lint_error.filename, []) + [lint_error]
+            info = re.search(r'\./(?P<path>.*?):\d*:\d*: *(?P<error>.*)', line).groupdict()
+            lint_error = LintError(info['path'], info['error'])
+        else:
+            lint_error.add_comment_line(line)
+    if lint_error:
+        errors[lint_error.filename] = errors.get(lint_error.filename, []) + [lint_error]
 
-
-def generate_comments(outs):
-    splitted_outs = outs.split('\n')
-    lines_with_errors = {}
-    for errors_index in range(0, len(splitted_outs), 3):
-        if splitted_outs[errors_index]:
-            info = re.search(r'(?P<path>.*?):(?P<line>\d*):\d*: *(?P<error>.*)',
-                             splitted_outs[errors_index])
-            if info:
-                info_dict = info.groupdict()
-                lwe = lines_with_errors.get(info_dict['line'], {})
-                lwe['errors'] = lwe.get('errors', []) + [info_dict['error']]
-                lwe['code'] = splitted_outs[errors_index + 1].strip()
-                lines_with_errors[info_dict['line']] = lwe
-    return lines_with_errors
+    return errors, bool(returncode)
 
 
 def main():
     print('::group::Flake8')
-    outs, returncode = run_flake()
-    lines_with_errors = generate_comments(outs)
+    errors, fail = run_flake()
 
     gh = Github(os.environ['GITHUB_TOKEN'])
     repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
@@ -51,17 +77,16 @@ def main():
         comments = [comment for comment in pr.get_comments() if comment.user.login == 'github-actions[bot]']
 
         for file in pr.get_files():
-            if file.status == 'removed':
-                continue
-            for diff_index, diff_code in enumerate(file.patch.split('\n')):
-                if diff_code[0] == '+':
-                    for lwe in lines_with_errors.values():
-                        if lwe['code'] == diff_code[1:].strip():
-                            body = '\n'.join(lwe['errors'])
-                            if not already_commented(file, diff_index, body, comments):
-                                pr.create_review_comment(body, commit, file.filename, diff_index)
+            if file.filename in errors:
+                for diff_index, diff_code in enumerate(file.patch.split('\n')):
+                    if diff_code[0] == '+':
+                        for error in errors[file.filename]:
+                            if error.code == diff_code[1:]:
+                                body = error.comment
+                                if not already_commented(file, diff_index, body, comments):
+                                    pr.create_review_comment(body, commit, file.filename, diff_index)
     print('::endgroup::')
-    exit(returncode)
+    exit(fail)
 
 
 main()
