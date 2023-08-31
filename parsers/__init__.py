5bbe26f00df1fade351be491fd1858bdfb6ec890
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-08-31 17:08:51 -0300

    refactor

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -2,12 +2,10 @@ from .parser import LinterParser
 from .shfmt import ShfmtParser
 from .flakeeight import Flake8Parser
 from .shellcheck import ShellCheckParser
-from .black import BlackParser
 
 __all__ = [
     "LinterParser",
     "ShfmtParser",
     "Flake8Parser",
     "ShellCheckParser",
-    "BlackParser",
 ]
